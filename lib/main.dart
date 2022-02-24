3237016a76cd1ca928b3e7df8a917dc73be2d09f
PAUL OKEYO <paulokeyo@hotmail.com>
2022-02-24 12:40:05 +0300

    Chapter two

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(
-        primarySwatch: Colors.brown),
+    theme: ThemeData(primarySwatch: Colors.brown),
   ));
 }
 
@@ -17,8 +16,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text('Hello world'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [Colors.black, Colors.white]),
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)]),
+          alignment: Alignment.center,
+          child: Text('I am a box',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold)),
+        ),
       ),
     );
   }
